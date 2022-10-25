0f781aef8bd60bd887958bb377f72c941e79a984
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2022-10-24 23:09:37 -0300

    projeto fullstack

---
+++ full stack React, node e react native/01-prog.js
@@ -0,0 +1,9 @@
+let nome = prompt(`what's your name? `)
+
+let sobrenome = prompt(`What's you lastname? `)
+
+alert(`My name is ${nome} ${sobrenome}`)
+
+document.write(`<h1>Bem vindo ao projeto ${nome} ${sobrenome} </h1>`)
+
+document.write(`<img src='https://sujeitoprogramador.com/steve.png' alt='foto do steve' />`)
---
+++ full stack React, node e react native/conversões.js
@@ -0,0 +1,10 @@
+let x = 34.9775
+
+binaryString = x.toString(2)
+
+octal_string = "O" + x.toString(8)
+hex_string = "Ox" + x.toString(16)
+
+x.toPrecision(5)
+
+console.log(x)
