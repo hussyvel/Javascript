6a59b071a960f9357f48a7c9413db00705b7faf2
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2022-11-16 18:42:05 -0300

    Comparacao de operadores

---
+++ Javascript Puro do zero ao mestre/js/operadoresComparacao.js
@@ -0,0 +1,5 @@
+let idade = 21;
+let paisPresentes = false;
+
+const podeViajar = idade >= 18 || paisPresentes;
+console.log(`Pode viajar: ${podeViajar}`)
---
+++ Javascript Puro do zero ao mestre/js/while.js
@@ -0,0 +1,6 @@
+let laco = 0
+
+while(laco < 10){
+    console.log(laco)
+    laco++;
+}
