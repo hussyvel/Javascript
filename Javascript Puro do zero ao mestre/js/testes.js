4a48c5913da8f4ccde9681b3f61dc435c80ad113
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2022-12-01 14:38:44 -0300

    Adicionando features na branch do projeto Javascript Puro

--- Javascript Puro do zero ao mestre/js/testes.js
+++ Javascript Puro do zero ao mestre/js/testes.js
@@ -1,10 +1,6 @@
-function somar(n1 = 0, n2 = 0){
-    //let na = n1 || 0
-    //let ne = n2 || 0
-    return n1 + n2
+function expression(){
+    console.log(`Hi, good afternoon. How it's goin?`)
 }
 
-let soma = somar(10, 9, 1)
-console.log(soma)
+expression()
 
-console.log(somar())
